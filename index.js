f2c92ffd70fa54949fc8d391e033a9cabdee8ebe
yuliyambg <yuliyambg@gmail.com>
2021-02-18 19:18:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+
+function superbowlWin(games) {
+  const win = games.find(game => {
+    return game.result === "W"
+  })
+
+  if(win !== undefined){
+    return win.year
+  }
+
+  return undefined
+
+  // return win === undefined ? undefined: win.year
 }
