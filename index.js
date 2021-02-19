50c38544a73436a798c45a0e80a6ecf89800caf2
yuliyambg <yuliyambg@gmail.com>
2021-02-18 19:15:54 -0500

    commit tree ba732fbf35a25c91c74bcda09604aec5e60092dc

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+
+function superbowlWin(games) {
+  const win = games.find(function(game) {
+    return game.result === "W"
+  })
+
+  return win === undefined ? undefined: win.year
+  // if(win !== undefined){
+  //   return win.year
+  // }
+  //
+  // return undefined
 }
